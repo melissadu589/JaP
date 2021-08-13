6de19d97c7bb747d1dc0cb935ee9dfc79443402d
melissadur589 <melissaduarte589@gmail.com>
2021-08-13 10:52:52 -0300

    los cambios

--- js/products.js
+++ js/products.js
@@ -4,3 +4,36 @@
 document.addEventListener("DOMContentLoaded", function (e) {
 
 });
+
+
+
+async function productos(){
+  let promise = await fetch ('https://japdevdep.github.io/ecommerce-api/product/all.json')
+    let data = await promise.json()
+
+    let products = '';
+
+for (let index =0; index <data.length; index++) {
+         products += 
+      `${data[index].name}
+        ${data[index].description}
+   ${data[index].cost}
+       ${data[index].currency}
+       ${data[index].imgSrc}
+       ${data[index].soldCount}`
+        
+} 
+
+
+ 
+let contenido = 
+` ${products}`
+
+
+ 
+let nuevo_elemento = document.createElement("div");
+nuevo_elemento.setAttribute("Id", "respuesta");
+document.body.appendChild(nuevo_elemento);
+document.getElementById("respuesta").innerHTML = contenido; 
+};
+productos();
